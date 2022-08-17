9910888f8c3c9e0be95d05b855f7d1fd7d82b39b
AlexP09 <75215512+AlexP09@users.noreply.github.com>
2022-08-17 16:13:38 +0300

    Update with ErrorRaising, Exceptions and Tests

--- project.py
+++ project.py
@@ -1,60 +1,97 @@
-# WordCloud of an IMDb list 
-from imdb import Cinemagoer
+# WordCloud of an IMDb list
+from imdb import Cinemagoer, IMDbError
 import csv
 import wordcloud
 import matplotlib.pyplot as plt
 import sys
 import re
+
 # create an instance of the Cinemagoer class
-ia = Cinemagoer()
-results=[] #blank variable
+try:
+    ia = Cinemagoer()
+except IMDbError as error:
+    print("Something went wrong. Please contact project creator.")
+results = []  # blank variable
 # Welcome the user, obtaing the input, and call the rest of the functions
 def main():
-    print("Have you ever been curious to see what your favourite movies or tv-shows have in common?\nNow you can visualize the main topics that your motion pictures share!")
-    prompt=input("Please input a public list URL: ")
-    store_source_data(get_list(prompt)) 
-    read_source_data() 
+    print(
+        "Have you ever been curious to see what your favourite movies or tv-shows have in common?\nNow you can visualize the main topics that your motion pictures share!"
+    )
+    prompt = input("Please input a public list URL: ")
+    store_source_data(get_list(prompt))
+    read_source_data()
     build_wordcloud(read_source_data())
     print("Done!")
+
+
 # Receives the list provided by the user, makes sure that the list has the correct format using regex and storing the IMDb data to the blank variable
 def get_list(Imdblist):
-    if not re.search(r"ls[0-9]*",Imdblist):
-        sys.exit("Invalid format\nYou may want to check and see if your link contains a string starting with ls")
+    if not re.search(r"ls[0-9]*", Imdblist):
+        sys.exit(
+            "Invalid format\nYou may want to check and see if your link contains a string starting with ls"
+        )
     else:
-        listinput=re.search(r"(ls[0-9]*)",Imdblist).group(1)
-    results = ia.get_movie_list(listinput)
-    return results
+        listinput = re.search(r"(ls[0-9]*)", Imdblist).group(1)
+    try:
+        results = ia.get_movie_list(listinput)
+        return results
+    except IMDbError as error:
+        print("Something went wrong. Please contact project creator.")
+
+
 # Writes a csv where we will store the name and description of the elements in the list provided by the user(parsing with Cinemagoer)
 def store_source_data(IMDBlist):
-    with open("plots.csv", "w", newline='') as file: 
-     writer=csv.DictWriter(file, fieldnames=["movie","plot"]) 
-     writer.writerow({"movie": "Movie", "plot": "Plot"}) 
-     for i in IMDBlist:
-        MovieTitle=ia.get_movie(i.movieID) 
-        Description=MovieTitle['plot'][0] 
-        writer.writerow({"movie":MovieTitle,"plot":Description}) 
+    if IMDBlist==[]:
+        sys.exit(
+            "No elements could be retrieved from the URL given. Please make sure that the list is public!"
+        )
+    with open("plots.csv", "w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=["movie", "plot"])
+        writer.writerow({"movie": "Movie", "plot": "Plot"})
+        for i in IMDBlist:
+            try:
+                MovieTitle = ia.get_movie(i.movieID)
+                Description = MovieTitle["plot"][0]
+                writer.writerow({"movie": MovieTitle, "plot": Description})
+            except IMDbError as error:
+                print("Something went wrong. Please contact project creator.")
+            except KeyError:
+                Description=""
+
+
 # Open the csv file and retrieves only the descriptions (at this point) and stores them in a long string-necessary for wordcloud input
 def read_source_data():
-    description=""
-    with open ("plots.csv", "r", newline='') as datasource: #opens the file saved beforehand
-                reader=csv.DictReader(datasource) #variable for library to which we pass the temporary file
-                for line in reader: #reiterates over line in csv-each new line is a dictionary
-                    description +=" "+line['Plot'].lower()+" "
+    description =""
+    with open(
+        "plots.csv", "r", newline=""
+    ) as datasource:  # opens the file saved beforehand
+        reader = csv.DictReader(
+            datasource
+        )  # variable for library to which we pass the temporary file
+        for line in reader:  # reiterates over line in csv-each new line is a dictionary
+            description += " " + line["Plot"].lower() + " "
     return description
+
+
 # Builidng the wordcloud file using as few parameters due to code running time
-def build_wordcloud(words):   
-    wordcloudMovies=wordcloud.WordCloud(width = 1000, height = 1000,
-                background_color =None,
-                stopwords = None,
-                mode="RGBA",
-                min_font_size = 5).generate(words).to_file("Image.png")
+def build_wordcloud(words):
+    wordcloudMovies = (
+        wordcloud.WordCloud(
+            width=1000,
+            height=1000,
+            background_color=None,
+            stopwords=None,
+            mode="RGBA",
+            min_font_size=5,
+        )
+        .generate(words)
+        .to_file("Image.png")
+    )
     plt.figure()
     plt.imshow(wordcloudMovies, interpolation="bilinear")
     plt.axis("off")
     plt.show()
 
+
 if __name__ == "__main__":
     main()
-
-
-
--- test_project.py
+++ test_project.py
@@ -1,2 +1,19 @@
 #Your test functions must be in a file called test_project.py, which should also be in the “root” of your project.
 # Be sure they have the same name as your custom functions, prepended with test_ (test_custom_function, for example,
+import project
+import pytest
+
+   
+def test_get_list():
+    with pytest.raises(SystemExit):
+        project.get_list("https://www.imdb.com/user/ur41041927/ratings?ref_=nv_usr_rt_4")
+    with pytest.raises(SystemExit):
+        project.get_list("https://www.imdb.com/user/ur41041927/watchlist?ref_=nv_usr_wl")
+def test_store_source_data():
+    with pytest.raises(SystemExit):
+        project.store_source_data([])
+def test_read_source_data():
+    assert project.read_source_data()==""
+def test_build_wordcloud():
+    with pytest.raises(ValueError):
+        project.build_wordcloud("")
